026fd12d1f794605a3d6b9895cd1f3c58f954f3f
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-21 02:38:16 +0000

    Added answer.php

--- index.php
+++ index.php
@@ -1,9 +1,60 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The Address program" />
+  <meta name="keywords" content="mths, ics2o" />
+  <meta name="author" content="Nathan De Silva" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Address webpage made with JavaScript</title>
+</head>
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <script src="./js/script.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Address, with JS file</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/street.jpg" alt="street">
+      </div>
+      <div class="page-content">
+        <p style="font-size: x-large">Please enter the following:</p>
+      </div>
+      <br />
+    <form action="answer.php" method="POST">
+          <p>Street Number</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="street-number">
+            <label class="mdl-textfield__label" for="number-input">Street Number ...</label>
+          </div>
+          <br />
+          <p>Street Name</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text"  name="street-name">
+            <label class="mdl-textfield__label" for="name-input">Street Name ...</label>
+            <span class="mdl-textfield__error">Input is not a name!</span>
+          </div>
+          <br />
+          <!-- Accent-colored raised button with ripple -->
+          <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+        </form>
+      <br />
+      <div class="page-content-answer">
+        <div id="user-info">
+          Your address is:
+        </div>
+    </main>
+  </div>
+</body>
 </html>
