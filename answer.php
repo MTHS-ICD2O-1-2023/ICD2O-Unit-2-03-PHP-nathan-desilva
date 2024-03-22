43f08c19643c3dc6b2fc8d2e83e1079eb7471e5c
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-22 18:06:47 +0000

    formatted

--- answer.php
+++ answer.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <meta name="description" content="The Hello World program, in PHP" />
-  <meta name="keywords" content="mths, ics2o" />
-  <meta name="author" content="Mr. Coxall" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./css/style.css" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-  <link rel="manifest" href="site.webmanifest" />
-  <title>The Address webpage made with PHP</title>
-</head>
-<body>
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Address program, in PHP</span>
-      </div>
-    </header>
-    <main class="mdl-layout__content">
-      <div class="right-image">
-        <img src="./images/street.jpg" alt="street image" width="250" />
-      </div>
-      <div class="page-content-php">
-        <div id="user-info">
-          <?php
-          $streetnumber = $_POST["street-number"];
-          $streetname = $_POST["street-name"];
-          echo "Your address is: " . $streetnumber . ", " . $streetname . ".";
-          ?>
+    <meta charset="utf-8" />
+    <meta name="description" content="The Hello World program, in PHP" />
+    <meta name="keywords" content="mths, ics2o" />
+    <meta name="author" content="Mr. Coxall" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <title>The Address webpage made with PHP</title>
+  </head>
+  <body>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">The Address program, in PHP</span>
         </div>
-      </div>
-    </main>
-  </div>
-</body>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="right-image">
+          <img src="./images/street.jpg" alt="street image" width="250" />
+        </div>
+        <div class="page-content-php">
+          <div id="user-info">
+            <?php
+            $streetnumber = $_POST["street-number"];
+            $streetname = $_POST["street-name"];
+            echo "Your address is: " . $streetnumber . ", " . $streetname . ".";
+            ?>
+          </div>
+        </div>
+      </main>
+    </div>
+  </body>
 </html>
